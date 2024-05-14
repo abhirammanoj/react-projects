e1fdb087c38d2ec87b265cd822ec81bf64fb1e4e
abhirammanoj <abhirammanojabhayam@gmail.com>
2024-05-14 23:23:33 +0530

    push to develop

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,18 @@
 function ListGroup() {
-  const items = ["Kerala", "Tamil Nadu", "Telgana", "Karnataka", "Hyderabad"];
+  let items = ["Kerala", "Tamil Nadu", "Telgana", "Karnataka", "Hyderabad"];
+  // items = [];
+
+  const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p> : null;
+  };
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li>{item}</li>
+          <li key={item}>{item}</li>
         ))}
         ;
       </ul>
